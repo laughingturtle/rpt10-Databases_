3200555ddcd35b8182dc1dcf2e49c9dc79b1563f
anand <anandxpatel@gmail.com>
2018-10-28 19:49:43 -0400

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,39 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  user_id INT NOT NULL PRIMARY KEY,
+  user_name VARCHAR(30) NOT NULL
+
 );
 
-/* Create other tables and define schemas for them here! */
+DROP TABLE IF EXISTS rooms;
 
+CREATE TABLE rooms (
+  room_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  room_name VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE messages (
+  message_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  message_text VARCHAR(146) NOT NULL,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  FOREIGN KEY (user_id)
+      REFERENCES users (user_id)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
+  FOREIGN KEY (room_id)
+      REFERENCES rooms (room_id)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT
+);
 
 
 
