0164f1ee7e7ce412baece33d8d19533ffec09851
anand <anandxpatel@gmail.com>
2018-10-31 19:00:53 -0400

    Fixed user post and get.

--- client/scripts/refactor.js
+++ client/scripts/refactor.js
@@ -22,7 +22,7 @@
 
 app = {
 
-  server: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+  server: 'http://127.0.0.1:3000/classes/messages',
 
   init: function() {
     // Get username
@@ -76,7 +76,7 @@ app = {
   loadMsgs: function() {
     $.ajax({
       url: app.server,
-      data: { order: '-createdAt' },
+      // datsa: { order: '-createdAt' },
       contentType: 'application/json',
       success: function(json) {
         app.displayMessages(json.results);
